6e22a36ba9d514a3f3cc74ee36c8266f2e0151ad
alperaltuntas <alperaltuntas@gmail.com>
2019-05-07 20:35:00 -0600

    Add orderedDict

--- tests/check_default_params.py
+++ tests/check_default_params.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from collections import OrderedDict
 import json
 import yaml
 
 # Check if yaml and json files are equivalent
 yaml_params = yaml.safe_load(open('./param_templates/default_params.yaml', 'r'))
-json_params = json.load(open('./param_templates/json/default_params.json', 'r'))
+json_params = json.load(open('./param_templates/json/default_params.json', 'r'),
+                        object_pairs_hook=OrderedDict)
 assert (yaml_params==json_params), "default_params.yaml and .json files appear to have different "+\
                                    "information. If you have updated yaml file but not json file, "+\
                                    "update it by running param_templates/yaml_to_json.py and then "+\
