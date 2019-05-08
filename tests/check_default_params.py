ab5ef126c80107c8dae8c1951a471462648981d8
alperaltuntas <alperaltuntas@gmail.com>
2019-05-07 19:55:59 -0600

    initializing travis

--- tests/check_default_params.py
+++ tests/check_default_params.py
@@ -1,2 +1,9 @@
+from __future__ import print_function
+
 print("travis CI initial test")
-print("travis CI initial test")
+
+try:
+    import yaml
+    print("yaml module exists")
+except:
+    print("no yaml module")
