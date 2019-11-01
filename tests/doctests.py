93721d5d0113ec5562c18b27acd924d9979f7720
alperaltuntas <alperaltuntas@gmail.com>
2019-11-01 17:05:41 -0600

    add doctests

---
+++ tests/doctests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os, sys
+import unittest
+from doctest import testmod 
+
+sys.path.append(os.path.join("../","cime_config","MOM_RPS"))
+import rps_utils
+
+if __name__ == '__main__': 
+    testmod(rps_utils, verbose=True)
