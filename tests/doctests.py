e968908eba04aa8db55b645ea55761e8785d804f
alperaltuntas <alperaltuntas@gmail.com>
2019-11-01 17:08:44 -0600

    fix doctests

--- tests/doctests.py
+++ tests/doctests.py
@@ -4,7 +4,9 @@ import os, sys
 import unittest
 from doctest import testmod 
 
+sys.path.append(os.path.join("cime_config","MOM_RPS"))
 sys.path.append(os.path.join("../","cime_config","MOM_RPS"))
+
 import rps_utils
 
 if __name__ == '__main__': 
