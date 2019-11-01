6e31edd9f942bfb1365a30e26363d16bd16cb3c5
alperaltuntas <alperaltuntas@gmail.com>
2019-11-01 17:35:38 -0600

    finish up docstring of rps_utils

--- cime_config/MOM_RPS/rps_utils.py
+++ cime_config/MOM_RPS/rps_utils.py
@@ -1,21 +1,35 @@
-""" Utilities for MOM_RPS module"""
+"""Auxiliary functions to be used in MOM_RPS."""
 
 from __future__ import print_function
 from collections import OrderedDict
 import os
 import re
 
-
-def is_number(val):
+def is_number(var):
     """
-    Alternative to isnumeric(), which can't handle scientific notation
+    Returns True if the passed var (of type string) is a number. Returns
+    False if var is not a string or if it is not a number.
+    This function is an alternative to isnumeric(), which can't handle
+    scientific notation.
+
+    Parameters
+    ----------
+    var: str
+        variable to check whether number or not
+
+    Returns
+    -------
+    True or False
+
+    >>> "1e-6".isnumeric()
+    False
     >>> is_number("1e-6") and is_number(1) and is_number(3.14)
     True
     >>> is_number([1,2]) or is_number("hello")
     False
     """
     try:
-        float(val)
+        float(var)
     except ValueError:
         return False
     except TypeError:
@@ -23,38 +37,59 @@ def is_number(val):
     return True
 
 def get_str_type():
+    """Returns the base string type, which depends on the major Python version."""
+
     try: # Python 2
         str_type = basestring
     except NameError: # Python 3
         str_type = str
     return str_type
 
-def is_logical_expr(string):
+def is_logical_expr(expr):
     """
     Returns true if a string is a logical expression.
+
+    Parameters
+    ----------
+    expr: str
+        expression to check whether logical or not
+
+    Returns
+    -------
+    True or False
 
     >>> is_logical_expr("0 > 1000")
     True
     """
 
     evaluated_val = None
-    if string.strip() == "else":
+    if expr.strip() == "else":
         evaluated_val = True
     else:
         try:
-            evaluated_val = eval(string)
+            evaluated_val = eval(expr)
         except (NameError):
-            pass # not a string to evaluate.
+            pass # not an expr to evaluate.
     return type(evaluated_val) == type(True)
 
-def has_expandable_var(entry):
+def has_expandable_var(expr):
     """
-    Checks if a given entry of type string has an expandable variable
+    Checks if a given expression has an expandable variable, e.g., $OCN_GRID.
+
+    Parameters
+    ----------
+    expr: str
+        expression to check
+
+    Returns
+    -------
+    True or False
 
     >>> has_expandable_var("${OCN_GRID} == tx0.66v1")
     True
     """
-    if isinstance(entry,get_str_type()) and "$" in entry:
+
+    if isinstance(expr,get_str_type()) and "$" in expr:
         return True
     else:
         return False
@@ -62,12 +97,21 @@ def has_expandable_var(entry):
 
 def _check_comparison_types(formula):
     """
-    A check to prevent the comparison of different data types. This function
+    A check to detect the comparison of different data types. This function
     replaces equality comparisons with order comparisons to check whether
     any variables of different types are compared. From Python 3.6 documentation:
     A default order comparison (<, >, <=, and >=) is not provided; an attempt
     raises TypeError. A motivation for this default behavior is the lack of a
     similar invariant as for equality.
+
+    Parameters
+    ----------
+    formula: str
+        formula to check if it includes comparisons of different data types
+
+    Returns
+    -------
+    True (or raises TypeError)
 
     >>> _check_comparison_types("3.1 > 3")
     True
@@ -87,7 +131,16 @@ def _check_comparison_types(formula):
 def eval_formula(formula):
     """
     This function evaluates a given formula and returns the result. It also
-    carries out sanity checks before evaluation.
+    carries out several sanity checks before evaluation.
+
+    Parameters
+    ----------
+    formula: str
+        formula to evaluate
+
+    Returns
+    -------
+    eval(formula)
 
     >>> eval_formula("3*5")
     15
