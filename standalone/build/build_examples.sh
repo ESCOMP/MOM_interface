fffb1ba96cd5b9bda3eaf92843b335cef6a8f275
Michael Levy <mike.levy.work@gmail.com>
2022-12-07 16:38:24 -0700

    Modified organization of build directory
    Dropped ocean_only from script name, and also now build into {COMPILER}/FMS and
    {COMPILER}/MOM6 instead of {COMPILER}/shared/repro and
    {COMPILER}/ocean_only/repro
    
    This means the command to actually run the executable from one of the examples/
    subdirectories is
    
    mpiexec_mpt dplace -s 1 ../../../build/intel/MOM6/MOM6

--- standalone/build/build_examples.sh
+++ standalone/build/build_examples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 ### For NCAR machines, this script should be run as
-### $ qcmd -- ./build_ocean_only_examples.sh
+### $ qcmd -- ./build_examples.sh
 
 # Save various paths to use as shortcuts
 cd ../..
@@ -19,16 +19,16 @@ COMPILER=intel
 
 #    (b) build FMS
 cd ${INTERFACE_ROOT}/standalone/build
-mkdir -p ${COMPILER}/shared/repro
-cd ${COMPILER}/shared/repro
+mkdir -p ${COMPILER}/FMS
+cd ${COMPILER}/FMS
 ${MKMF_ROOT}/list_paths ${FMS_ROOT}/src
 ${MKMF_ROOT}/mkmf -t ${TEMPLATE_DIR}/cheyenne-${COMPILER}.mk -p libfms.a -c "-Duse_libMPI -Duse_netCDF -DSPMD" path_names
-make NETCDF=3 REPRO=1 libfms.a -j
+make -j36 NETCDF=3 REPRO=1 libfms.a
 
 # 2) Build MOM6
 cd ${INTERFACE_ROOT}/standalone/build
-mkdir -p ${COMPILER}/ocean_only/repro
-cd ${COMPILER}/ocean_only/repro
+mkdir -p ${COMPILER}/MOM6
+cd ${COMPILER}/MOM6
 ${MKMF_ROOT}/list_paths -l ${MOM_ROOT}/{config_src/infra/FMS2,config_src/memory/dynamic_symmetric,config_src/drivers/solo_driver,config_src/external,src/{*,*/*}}/
-${MKMF_ROOT}/mkmf -t ${TEMPLATE_DIR}/cheyenne-${COMPILER}.mk -o '-I../../shared/repro' -p MOM6 -l '-L../../shared/repro -lfms' -c '-Duse_libMPI -Duse_netCDF -DSPMD' path_names
-make NETCDF=3 REPRO=1 MOM6 -j
+${MKMF_ROOT}/mkmf -t ${TEMPLATE_DIR}/cheyenne-${COMPILER}.mk -o '-I../FMS' -p MOM6 -l '-L../FMS -lfms' -c '-Duse_libMPI -Duse_netCDF -DSPMD' path_names
+make -j36 NETCDF=3 REPRO=1 MOM6
