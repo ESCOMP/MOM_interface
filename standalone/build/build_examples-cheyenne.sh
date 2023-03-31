e66025ffb00e2df06e6f90a195044e91ddad5417
Michael Levy <mike.levy.work@gmail.com>
2023-03-31 12:46:38 -0600

    Add support for submitting build script to PBS
    Also added ability to override the default directory name that just matches
    compiler choice (so Cheyenne can build to intel-cheyenne/ while Casper builds
    to intel-casper/)

--- standalone/build/build_examples-cheyenne.sh
+++ standalone/build/build_examples-cheyenne.sh
@@ -2,6 +2,8 @@
 ### For NCAR machines, this script should be run as
 ### $ qcmd -- ./build_examples.sh
 
+echo "Starting build at `date`"
+
 # Save various paths to use as shortcuts
 cd ../..
 INTERFACE_ROOT=`pwd -P`
@@ -16,20 +18,28 @@ SHR_ROOT=${CESM_ROOT}/components/cdeps/share
 COMPILER=intel
 TEMPLATE=${TEMPLATE_DIR}/cheyenne-${COMPILER}.mk
 
+if [ -e $1 ]; then
+  BLD_ROOT=${COMPILER}
+else
+  BLD_ROOT=$1
+fi
+
 # 1) Build FMS
 
 #    (b) build FMS
 cd ${INTERFACE_ROOT}/standalone/build
-mkdir -p ${COMPILER}/FMS
-cd ${COMPILER}/FMS
+mkdir -p ${BLD_ROOT}/FMS
+cd ${BLD_ROOT}/FMS
 ${MKMF_ROOT}/list_paths ${FMS_ROOT}/src
 ${MKMF_ROOT}/mkmf -t ${TEMPLATE} -p libfms.a -c "-Duse_libMPI -Duse_netCDF -DSPMD" path_names
 make -j36 NETCDF=3 REPRO=1 libfms.a
 
 # 2) Build MOM6
 cd ${INTERFACE_ROOT}/standalone/build
-mkdir -p ${COMPILER}/MOM6
-cd ${COMPILER}/MOM6
+mkdir -p ${BLD_ROOT}/MOM6
+cd ${BLD_ROOT}/MOM6
 ${MKMF_ROOT}/list_paths -l ${MOM_ROOT}/{config_src/infra/FMS2,config_src/memory/dynamic_symmetric,config_src/drivers/solo_driver,pkg/MARBL/src,config_src/external,src/{*,*/*}}/
 ${MKMF_ROOT}/mkmf -t ${TEMPLATE} -o '-I../FMS' -p MOM6 -l '-L../FMS -lfms' -c '-Duse_libMPI -Duse_netCDF -DSPMD' path_names
 make -j36 NETCDF=3 REPRO=1 MOM6
+
+echo "Finished build at `date`"
